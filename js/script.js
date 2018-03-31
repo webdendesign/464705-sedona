641d2053c6d78639953916f619e8c85f2e090933
Den <fabri.din@yandex.ru>
2018-03-31 23:58:35 +0500

    однообразие

--- js/script.js
+++ js/script.js
@@ -1,25 +1,22 @@
+var search = document.querySelector(".open-form-search");
+var popup = document.querySelector(".modal");
+var form = popup.querySelector("form");
+var arrival_date = popup.querySelector("[name=come]");
+var departure_date = popup.querySelector("[name=ride-out]");
+var parent = popup.querySelector("[name=number-one]");
+var children = popup.querySelector("[name=number-two]");
 
-      var search = document.querySelector(".open-form-search");
+popup.classList.add("modal-show");
 
-      var popup = document.querySelector(".modal");
+search.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("modal-show");
+  arrival_date.focus();
+});
 
-      var form = popup.querySelector("form");
-      var arrival_date = popup.querySelector("[name=come]");
-      var departure_date = popup.querySelector("[name=ride-out]");
-      var parent = popup.querySelector("[name=number-one]");
-      var children = popup.querySelector("[name=number-two]");
-
-      popup.classList.add("modal-show");
-
-      search.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        popup.classList.toggle("modal-show");
-        arrival_date.focus();
-      });
-
-      form.addEventListener("submit", function (evt) {
-        if (!arrival_date.value || !departure_date.value || !parent.value || !children.value) {
-          evt.preventDefault();
-          console.log("Нужно ввести даты приезда и отъезда и количество отдыхающих");
-        }
-      });
+form.addEventListener("submit", function (evt) {
+  if (!arrival_date.value || !departure_date.value || !parent.value || !children.value) {
+    evt.preventDefault();
+    console.log("Нужно ввести даты приезда и отъезда и количество отдыхающих");
+  }
+});
